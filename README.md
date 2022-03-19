f4b3aac6dac29b3008930b643685002d8e22546f
Ola <ol.holovina@gmail.com>
2022-03-19 07:46:23 -0400

    base exampe works

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# weather-command-line-tool
+# Humber's assignment - simple node.js command line tool
+
+With this tool you can get the current air temperature in some specific city
+If there is no such city you will get an error
+All requests are stored in log.txt file
+command example: node app.js Toronto
--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-fetch(`https://weatherapi-com.p.rapidapi.com/forecast.json?q=${city}&days=1`, {
-  "method": "GET",
+const axios = require("axios").default;
+
+const town = process.argv[2];
+if (!town) {
+  console.log('Enter the town, please!');
+  process.exit();
+}
+
+const options = {
+  method: 'GET',
+  url: `https://weatherapi-com.p.rapidapi.com/forecast.json?q=${town}&days=1`,
   "headers": {
     "x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
     "x-rapidapi-key": "40da0fbf92mshfe7a448a5fbb967p17796ejsn5fe22c938fc1"
   }
-})
+};
+
+axios.request(options).then(function (response) {
+  console.log(response.data);
+}).catch(function (error) {
+  console.error(error);
+});
